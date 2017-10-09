f7ff3f70416296fccf9a123bdf23b01fa471c797
sr20det <12f8219@gmail.com>
2017-10-09 13:51:23 +0300

    hmmm

--- main.c
+++ main.c
@@ -8,18 +8,24 @@
 
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
+#define UT_RUN	0x6e7572LL
+#define UT_DONW 0x6e776f64LL
 
 typedef struct {
-	ino_t i;
-	pid_t p;
-	int n, f;
-} dUnit;
+	ino_t	i;
+	pid_t	p;
+	int		f;
+	union {
+		long long t;
+		char n[sizeof(long long)];
+	};
+} DUnit;
 
 int
-dread(DIR *dp, dUnit *du, int l)
+dread(DIR *dp, DUnit *du, const int l)
 {
 	struct dirent *de;
-	int i, z;
+	int i, z, n;
 
 	seekdir(dp, 1);
 	while ((de = readdir(dp)))
@@ -28,40 +34,47 @@ dread(DIR *dp, dUnit *du, int l)
 			continue;
 		for (z=i=0; i<l; i++)
 		{
+			if (du[i].i == de->d_ino)
+				break;
 			if (du[i].i == 0)
 				z = i;
-			else
-				if (du[i].i == de->d_ino)
-					break;
 		}
 		if (i == l && z)
 		{
 			du[z].i = de->d_ino;
 			du[z].f = !(de->d_type ^ DT_DIR);
-			strncpy((char*)&(du[z].n), de->d_name, sizeof(du[z].n));
+			n = MIN(strlen(de->d_name), sizeof(du[z].n));
+			strncpy(du[z].n, de->d_name, n);
 		}
 	}
 	return 0;
 }
 
 int
-runall(char *arg0, int argl, dUnit *du, int l)
+runall(char *arg0, int argl, DUnit *du, const int dL)
 {
 	int i;
 
 	argl = MIN(argl, sizeof(du[i].n));
-	for (i=0; i<l; i++)
+	for (i=0; i<dL; i++)
 	{
 		if (du[i].i == 0)
 			continue;
 		du[i].p = fork();
-		if (du[i].p == 0)
+		if (du[i].p != 0)
+			continue;
+		if (du[i].f == 1)
 		{
-			strncpy(arg0, (char*)&du[i].n, argl);
-			return 0;
+			strncpy(arg0, du[i].n, argl);
+			return 1;
+		}
+		switch (du[i].t)
+		{
+			case UT_RUN:
+				execl("run", arg0, NULL);
+				return 2;
 		}
 	}
-
 	return 0;
 }
 
@@ -74,15 +87,14 @@ main(int argc, char *argv[])
 	const int dL=256;
 	int i, argl;
 	pid_t p;
-	dUnit du[dL];
+	DUnit du[dL];
 	
 	argl = argv[argc-1] + strlen(argv[argc-1]) - argv[0];
 	dn	 = strdup( argc>1 ? argv[1] : "/var/sr" );
 	argc = 1;
 
  sr_0:
-	for (i=0; i<dL; i++)
-		memset(&du[i], 0, sizeof(dUnit));
+	memset(du, 0, sizeof(DUnit) * dL);
 	dp = opendir(dn);
 	if (dp == NULL)
 		return -1;
